5a2d472cdeaa13dbb66ccae8152fa358d4fb1d23
Kleo <mmttdd16@gmail.com>
2023-09-02 19:05:03 +0300

    ex1 and ex2 complete

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -7,5 +7,4 @@ System.Console.Write("Введите число: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 
 int number = int.Parse(Console.ReadLine()!);
-System.Console.Write("квадрат числа " + number + " -> " );
- System.Console.WriteLine(number*number);
+ System.Console.WriteLine("квадрат числа " + number + " -> " + number * number);
--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите первое число: ");
+int firsNum = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите второе число: ");
+int secNum = int.Parse(Console.ReadLine()!);
+
+if ( firsNum ==  secNum * secNum)
+{
+    System.Console.WriteLine(firsNum + " является квадратом " + secNum);
+}
+else 
+{
+    System.Console.WriteLine(firsNum + " не является квадратом " + secNum);
+}
