2787b63344347492a764b5e0403c6a4ae6182891
Kleo <mmttdd16@gmail.com>
2023-09-02 21:08:53 +0300

    Finished Ex5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// System.Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int lastDigit = num%10;
+// System.Console.WriteLine(lastDigit);
+// System.Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// if (num > 99 && num < 1000)
+// {
+//     int lastDigit = num%10;
+//     System.Console.WriteLine(lastDigit);
+
+// }
+// else
+// System.Console.WriteLine("Это не трех значное число");
+
+
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+// Используем Math.Abs() для нахождения модуля числа
+int absoluteNum = Math.Abs(num);
+
+if (absoluteNum > 99 && absoluteNum < 1000)
+{
+    int lastDigit = absoluteNum % 10;
+    System.Console.WriteLine("Последняя цифра: " + lastDigit);
+}
+else
+{
+    System.Console.WriteLine("Это не трехзначное число");
+}
