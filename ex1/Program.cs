99b429ea976da3db6d6ef5b45bce8ada9646f417
Kleo <mmttdd16@gmail.com>
2023-09-02 18:37:24 +0300

    modified ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// f11f12f1f
+// 12f1f12f
+// f11f12f1f12f1
+// 12
+
+System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int number = int.Parse(Console.ReadLine()!);
+System.Console.Write("квадрат числа " + number + " -> " );
+ System.Console.WriteLine(number*number);
